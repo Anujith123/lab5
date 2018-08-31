1dac2b20c26bdfcbdf6f163fcbd29891453e5166
Anujith123 <42373652+Anujith123@users.noreply.github.com>
2018-08-31 19:52:15 +0000

    Add files via upload

---
+++ lab5q25.cpp
@@ -0,0 +1,16 @@
+//including library
+#include<iostream>
+using namespace std;
+int main()
+{
+// assigning variables
+int i;
+cout<<"The even numbers between 1 and 100"<<endl;
+i=0;
+// loop
+while(i<100){
+i+=2;
+cout<<i<<",";
+}cout<<endl;
+return 0;
+}
