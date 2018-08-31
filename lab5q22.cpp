c5b8b61cd716855f38491dc2090a9950537443a8
Anujith123 <42373652+Anujith123@users.noreply.github.com>
2018-08-31 19:46:43 +0000

    Add files via upload

---
+++ lab5q22.cpp
@@ -0,0 +1,18 @@
+//including library
+#include<iostream>
+using namespace std;
+int main()
+{
+// assigning variables
+int i=1 ,n;
+cout<<"The number you want :"<<endl;
+cin>>n;
+// loop
+while(i<=n){
+// the code for printing the natural numbers
+cout<<"The number is : "<<i<<endl;
+i++;
+// adding 1 to predecessor
+}
+return 0;
+}
---
+++ lab5q24.cpp
@@ -0,0 +1,16 @@
+//including library
+#include<iostream>
+using namespace std;
+int main()
+{
+// assigning variables
+int i;
+cout<<"The English alphabetic letters are"<<endl;
+i=65; // providing ascii code
+// loop
+while(i<91){
+cout<<char (i);
+i++;
+cout<<endl;}
+return 0;
+}
